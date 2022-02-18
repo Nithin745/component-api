f97ef1f43112360ebc1d7dc866d5959b83a55dd5
kavisoftek <kavisoftek@pop-os.localdomain>
2022-02-18 18:13:25 +0530

    second commit

--- pages/models.py
+++ pages/models.py
@@ -11,8 +11,8 @@ class Type(models.Model):
 
 
 class Content(models.Model):
-    main_content = models.TextField(max_length=0, null=True)
-    sub_content = models.TextField(max_length=0, null=True)
+    main_content = models.TextField(max_length=500, null=True)
+    sub_content = models.TextField(max_length=500, null=True)
     image = models.ImageField(upload_to='images/', null=True)
     type = models.ForeignKey(
         Type, on_delete=models.CASCADE, related_name='content_type')
